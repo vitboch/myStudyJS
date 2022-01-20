1721618c938d12043a005ab5b2c1cb1272d459b4
VitBoch <vitaliybochkarev@mail.ru>
2022-01-20 09:40:41 +0700

    findWord.js update

---
+++ findWord.js
@@ -0,0 +1,8 @@
+phrase = "Hello my name is Juan"
+word = "Daniel"
+
+// todo: Next function must return true or false
+function findWord(phrase, word) {
+    /* TODO */
+    return phrase.includes(word);
+}
