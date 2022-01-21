1014070586c7628f9096da5d747d0daffa0c6e1a
VitBoch <vitaliybochkarev@mail.ru>
2022-01-21 08:23:16 +0700

    isPalindrome

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,7 +1,7 @@
 function isPalindrome(userStr) {
     for (let letter = 0; letter < userStr.length / 2; letter++) {
         if (userStr.charAt(userStr.length[0]) !== userStr
-            .charAt(userStr.length-1)) {
+            .charAt(userStr.length - 1)) {
             return false;
         }
     }
