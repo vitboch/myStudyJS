382a6d45decfa11ed3a3432bae1006195dfafd68
VitBoch <vitaliybochkarev@mail.ru>
2022-01-21 08:19:29 +0700

    isPalindrome

---
+++ isPalindrome.js
@@ -0,0 +1,12 @@
+function isPalindrome(userStr) {
+    for (let letter = 0; letter < userStr.length / 2; letter++) {
+        if (userStr.charAt(userStr.length[0]) !== userStr
+            .charAt(userStr.length-1)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("javascript"));
