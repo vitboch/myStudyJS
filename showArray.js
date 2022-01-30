6f8d6ec469fba6e09a565ec6499872103aca37e9
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-01-30 18:05:33 +0700

    first commit

---
+++ showArray.js
@@ -0,0 +1,9 @@
+const planets = ["Earth", "Jupiter", "Neptune"];
+
+function showArray(planets) {
+    planets.forEach(function(value) {
+        console.log(value);
+    });
+}
+
+showArray(planets);
