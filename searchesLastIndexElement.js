9a8546b2f420b281e9dd570f697fa7105651ca3c
VitBoch <vitaliybochkarev@mail.ru>
2022-02-24 14:04:46 +0700

    searchesLastIndexElement

---
+++ searchesLastIndexElement.js
@@ -0,0 +1,8 @@
+const results = [3, 5, 2, 1, 2, 3, 4, 6, 1, 5];
+
+function searchesLastIndexElement(element) {
+    return results.lastIndexOf(element);
+}
+
+let element = 5;
+console.log(searchesLastIndexElement(element));
